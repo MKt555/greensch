9541daa6441d57608e9ad456ce71327a9821e466
MKt555 <tmrwkamau@gmail.com>
2021-06-01 21:12:00 +0300

    security cchanges

--- OnlineLearning/settings.py
+++ OnlineLearning/settings.py
@@ -12,17 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
+import secrets.py
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*u%cov=^7^2u_)x7o%26bu(_=zhncflkgvgqy!=%+re(s=e=mh'
-
+#Adds Secret key cut away
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -80,22 +79,6 @@ WSGI_APPLICATION = 'OnlineLearning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'SchooWebDB',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'datadatabase0',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
-}
-}
 
 
 # Password validation
