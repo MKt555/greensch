79c1a7b1d7e170db35d0d125d2e58dffc76ab9f9
MKt555 <tmrwkamau@gmail.com>
2021-06-02 12:41:53 +0300

    gitignore changes

--- OnlineLearning/settings.py
+++ OnlineLearning/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-import secrets.py
+from my_secrets import secrets
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 #Adds Secret key cut away
-SECRET_KEY = config("SECRET_KEY")
+#SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     'main',
     'academy',
     'accounts',
-    'materializecssform'
+    'materializecssform',
+    'django_secrets',
 ]
 
 
